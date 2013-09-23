52fa913a250972c8ae8cee9d77dc95a76a19aa71
Sungjun Park <sjpark@codeaurora.org>
2013-09-23 14:16:47 -0700

    Bluetooth: Add SIBS Macro to enable or disable based on a target
    Based on a target, the bluetooth chipset can suppourt software
    Inband sleep or not. So, SIBS feature should be controllable
    to enable or disable base on a target
    
    Change-Id: Ib8c20017f2ddcd22005d361e3b2ad1045e386b4d
    CRs-fixed: 549989

--- hci/Android.mk
+++ hci/Android.mk
@@ -21,8 +21,12 @@ LOCAL_SRC_FILES += \
         src/userial.c \
         src/userial_mct.c \
         src/hci_mct.c \
-        src/hci_h4.c\
-        src/hci_ibs.c
+        src/hci_h4.c
+
+ifeq ($(QCOM_BT_USE_SIBS),true)
+LOCAL_SRC_FILES += src/hci_ibs.c
+LOCAL_CFLAGS += -DQCOM_BT_SIBS_ENABLE
+endif
 
 ifeq ($(BLUETOOTH_HCI_USE_USB),true)
 
